3b68c8509ba068f3d3c5b72fd28aaef53e834c2f
gonwe <gonwe@qq.com>
2022-04-20 14:43:32 +0800

    feat：前端项目分离后重新开发

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: "app",
+  mounted() {},
+};
+</script>
+<style lang='scss'>
+@import "./assets/style/reset.css";
+@import "./assets/style/index.scss";
+</style>
+
+
